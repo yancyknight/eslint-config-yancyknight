fc97682409d1f5f7c7e468b9d7f58509c223e7fb
Yancy Knight <yancy.knight@gmail.com>
2018-02-08 11:20:56 -0700

    Initial Commit

---
+++ index.js
@@ -0,0 +1,42 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "globals": {
+        "av": true,
+        "_wuiVar": true,
+        "jQuery": true,
+        "er": true
+    },
+    "extends": "eslint:recommended",
+    "rules": {
+        "indent": [
+            // "off"
+            "error",
+            4,
+            {
+                "outerIIFEBody": 0,
+                "ArrayExpression": "off",
+                "ObjectExpression": "off"
+            }
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ],
+        "no-unused-vars": [
+            "error",
+            {"args": "none"}
+        ],
+        "no-console": "off"
+    }
+}
